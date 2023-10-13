a4e8c2b2821ad9c84e43e07e361beadb2da902b0
Ubuntu <ubuntu@ip-172-31-95-36.ec2.internal>
2023-10-13 18:05:44 +0000

    Uopdated javaaaa

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class github {
   public static void main (String[] args){
-    System.out.println("Hello World")
+    System.out.println("Hello Worl")
+	    System.out.println("Ore Wa Pain ")
       }
 }
